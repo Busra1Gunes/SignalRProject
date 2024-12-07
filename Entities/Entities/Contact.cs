cca4fb6320652d34c1dbbd3739f25ddcd9705bb7
busragunes <busragns1999@hotmail.com>
2024-12-07 12:23:52 +0300

    Add project files.

---
+++ Entities/Entities/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class Contact //İletişim bilgileri
+    {
+        public int ContactID { get; set; }
+        public string Location { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        public string FooterDescription { get; set; }
+
+    }
+}
---
+++ Entities/Entities/Testimonial.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.Entities
+{
+    public class Testimonial  //Referanslar
+    {
+        public int TestimonialID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
